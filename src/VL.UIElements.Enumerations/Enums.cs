da01d4993f7c307ba1ccd1dcc2b92abb10841aeb
vvvv-dottore <natan.sinigaglia@gmail.com>
2019-05-14 18:27:03 +0200

    - Added Enumeration VS document
    - started prototyping a Style class

---
+++ src/VL.UIElements.Enumerations/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIElements.Core
+{
+
+    public enum Button_Shape
+    {
+        Rectangle,
+        Triangle_Up,
+        Triangle_Down,
+        Triangle_Left,
+        Triangle_Right,
+        Circle,
+        Menu
+    }
+
+}
