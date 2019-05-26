0f88bd3d50f9d9c83a90f3be11668d033daa6b5b
vvvv-dottore <natan.sinigaglia@gmail.com>
2019-05-26 20:16:41 +0200

    - implemented Style states as dictionary of Settings (datatypes)

--- src/VL.UIElements.Enumerations/Enums.cs
+++ src/VL.UIElements.Enumerations/Enums.cs
@@ -15,7 +15,38 @@ namespace UIElements.Core
         Triangle_Left,
         Triangle_Right,
         Circle,
+        RoundRectangle,
         Menu
+    }
+
+    public enum Interaction_State
+    {
+        Idle,
+        Hovered,
+        Focused,
+        Selected,
+        TextPreview
+    }
+
+    public enum Style_Element
+    {
+        Fill_Visible,
+        Fill_Color,
+        Fill_Paint,
+        Texture_Enable,
+        Texture,
+        Stroke_Visible,
+        Stroke_Color,
+        Stroke_Paint,
+        Shape,
+        Size,
+        Text_Visible,
+        Text_Color,
+        Text_Paint,
+        Text_Font,
+        Text_Size,
+        Text_Alignment_Vertical,
+        Text_Alignment_Horizontal,
     }
 
 }
