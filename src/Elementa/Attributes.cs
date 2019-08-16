054c88b21b42518f1e6cf19dc5a45a827f03b2ae
N-LAPTOP\natan <natan.sinigaglia@gmail.com>
2019-08-17 01:09:43 +0200

    - added attributes to VS project
    - Fixed StackY layout node

---
+++ src/Elementa/Attributes.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elementa.Attributes
+{
+    public class ExposeAttribute : Attribute
+    {
+    }
+
+    public class KnownWidgetAttribute : Attribute
+    {
+        public KnownWidgetAttribute(KnownWidget widget)
+        {
+            Widget = widget;
+        }
+
+        public KnownWidget Widget { get; }
+    }
+
+    public class WidgetOrientationAttribute : Attribute
+    {
+        public WidgetOrientationAttribute(WidgetOrientation widgetOrientation)
+        {
+            WidgetOrientation = widgetOrientation;
+        }
+
+        public WidgetOrientation WidgetOrientation { get; }
+    }
+
+    public class GroupExposureKindAttribute : Attribute
+    {
+        public GroupExposureKindAttribute(GroupExposureKind groupExposureKind)
+        {
+            GroupExposureKind = groupExposureKind;
+        }
+
+        public GroupExposureKind GroupExposureKind { get; }
+    }
+
+    public class CustomOSCAddressAttribute : Attribute
+    {
+        public CustomOSCAddressAttribute(string customOSCAddress)
+        {
+            CustomOSCAddress = customOSCAddress;
+        }
+
+        public string CustomOSCAddress { get; }
+    }
+
+    public class LayoutPriorityAttribute : Attribute
+    {
+        public LayoutPriorityAttribute(UInt32 layoutPriorityAttribute)
+        {
+            LayoutPriority = layoutPriorityAttribute;
+        }
+
+        public UInt32 LayoutPriority { get; }
+    }
+
+    // =======================================================================================
+    // VALUE RELATED ATTRIBUTES:
+
+    // Allowed types are bool, float, etc. string, enums, System.Type and array of previous OR object
+
+    public class DefaultValueAttribute : Attribute
+    {
+
+        public DefaultValueAttribute(object defaultValue)
+        {
+            DefaultValue = defaultValue;
+        }
+
+        public object DefaultValue { get; }
+    }
+
+    public class MininumValueAttribute : Attribute
+    {
+        public MininumValueAttribute(object minimumValue)
+        {
+            MinimumValue = minimumValue;
+        }
+
+        public object MinimumValue { get; }
+    }
+
+    public class MaximumValueAttribute : Attribute
+    {
+        public MaximumValueAttribute(object maximumValue)
+        {
+            MaximumValue = maximumValue;
+        }
+
+        public object MaximumValue { get; }
+    }
+
+    public class IsCyclicAttribute : Attribute
+    {
+    }
+
+    public class StepSizeAttribute : Attribute
+    {
+        public StepSizeAttribute(object stepSize)
+        {
+            StepSize = StepSize;
+        }
+
+        public object StepSize { get; }
+    }
+}
