3c7429f33cffc5367bfb11f83e50fb475c55ec21
sebescudie <seb.espm@gmail.com>
2020-03-26 13:30:52 +0100

    [lib] added Orientable to components enum

--- src/Elementa/Enum.cs
+++ src/Elementa/Enum.cs
@@ -24,7 +24,7 @@ namespace Elementa
 
     public enum ComponentType
     {
-        Interactive, Hoverable, Focusable, Selectable, Previewable, Typeable, Taggable, Styleable, Taskable, Moveable, Resizeable
+        Interactive, Hoverable, Focusable, Selectable, Previewable, Typeable, Taggable, Styleable, Taskable, Moveable, Resizeable, Orientable
     }
 
     public enum KnownWidget
