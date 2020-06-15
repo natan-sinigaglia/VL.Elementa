cbb824f1804c39425a9c31d6188cc52d61259d01
ravazquez <ravazquez@gmail.com>
2020-06-15 19:04:18 +0100

    Added entries for all know supported css properties

--- src/Elementa/CSSUtils.cs
+++ src/Elementa/CSSUtils.cs
@@ -25,6 +25,31 @@ namespace Elementa
                 case "color":
                     result = StyleProperties.Text_Color;
                     break;
+                case "font-family":
+                    result = StyleProperties.Text_FamilyName;
+                    break;
+                case "font-size":
+                    result = StyleProperties.Text_Size;
+                    break;
+                case "font-style":
+                    result = StyleProperties.Text_Style;
+                    break;
+                case "line-height":
+                    result = StyleProperties.Text_LineHeight;
+                    break;
+                case "padding":
+                case "padding-top":
+                case "padding-right":
+                case "padding-bottom":
+                case "padding-left":
+                    result = StyleProperties.Text_Padding;
+                    break;
+                case "text-align":
+                    result = StyleProperties.Text_HorizonalAlignment;
+                    break;
+                case "vertical-align":
+                    result = StyleProperties.Text_VerticalAlignment;
+                    break;
                 default:
                     break;
             }
